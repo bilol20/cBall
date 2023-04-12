efbffc9f1541b25f54d6395d2fdf486c32522c56
bilol20 <banerjeebilol@gmail.com>
2023-04-12 10:41:28 +0530

    update

--- R/Stat.R
+++ R/Stat.R
@@ -43,67 +43,67 @@ cBD = function(X,Y,Z, hz, hyz, kernel = c("normal", "epanenchnikov")){
   return(D)
 }
 
-cBD.test = function(X, Y, Z, beta = 0.5, R=500, kernel = c("normal", "epanenchnikov")){
-  if(class(X)[1]=="matrix"){
-    n = nrow(X)
-  }else{
-    n = length(X)
-  }
-  if(kernel == "normal"){
-    ker = ker.normal
-  }else{
-    if(kernel == "epanenchnikov"){
-      ker = ker.epa
-    }else{
-      print("Please specify the kernel correctly.")
-      exit
-    }
-  }
+#cBD.test = function(X, Y, Z, beta = 0.5, R=500, kernel = c("normal", "epanenchnikov")){
+#  if(class(X)[1]=="matrix"){
+#    n = nrow(X)
+#  }else{
+#    n = length(X)
+#  }
+#  if(kernel == "normal"){
+#    ker = ker.normal
+#  }else{
+#    if(kernel == "epanenchnikov"){
+#      ker = ker.epa
+#    }else{
+#      print("Please specify the kernel correctly.")
+#      exit
+#    }
+#  }
 
-  s = sample(1:n, size = as.integer(beta*n))
-  Xtr = X[s,]
-  Ytr = Y[s,]
-  Ztr = Z[s,]
+#  s = sample(1:n, size = as.integer(beta*n))
+#  Xtr = X[s,]
+#  Ytr = Y[s,]
+#  Ztr = Z[s,]
 
-  Xte = X[-s,]
-  Yte = Y[-s,]
-  Zte = Z[-s,]
+#  Xte = X[-s,]
+#  Yte = Y[-s,]
+#  Zte = Z[-s,]
 
-  n1 = length(s)
-  n2 = n-n1
-  hz = bw.selection(Xtr, Ztr, p1 = 0.4, p2 = 0.9)
-  hyz = bw.selection(Xtr, cbind(Ytr,Ztr), p1 = 0.3, p2 = 0.7)
+#  n1 = length(s)
+#  n2 = n-n1
+#  hz = bw.selection(Xtr, Ztr, p1 = 0.4, p2 = 0.9)
+#  hyz = bw.selection(Xtr, cbind(Ytr,Ztr), p1 = 0.3, p2 = 0.7)
 
-  Dx = as.matrix(dist(Xte))
-  Dz = dist(Zte)
-  Dyz = dist(cbind(Yte,Zte))
+#  Dx = as.matrix(dist(Xte))
+#  Dz = dist(Zte)
+#  Dyz = dist(cbind(Yte,Zte))
   #hz =  quantile(Dz,prob = 0.5)
   #hyz = quantile(Dyz,prob = 0.5)
 
-  Kz = matrix(sapply(as.matrix(Dz),function(x) ker(x,hz)),n2,n2)
-  Kyz = matrix(sapply(as.matrix(Dyz),function(x) ker(x,hyz)),n2,n2)
-  Wz = apply(Kz,1,sum)
-  Wyz = apply(Kyz,1,sum)
-  L = lapply(1:n2, function(i){
-    sapply(1:n2, function(j){
-      sapply(1:n2, function(k){
-        return(as.numeric(Dx[k,i]<Dx[j,i]))
-      })
-    })
-  })
-  D = CppS(n2,Kz,Wz,Kyz,Wyz,L)
+#  Kz = matrix(sapply(as.matrix(Dz),function(x) ker(x,hz)),n2,n2)
+#  Kyz = matrix(sapply(as.matrix(Dyz),function(x) ker(x,hyz)),n2,n2)
+#  Wz = apply(Kz,1,sum)
+#  Wyz = apply(Kyz,1,sum)
+#  L = lapply(1:n2, function(i){
+#    sapply(1:n2, function(j){
+#      sapply(1:n2, function(k){
+#        return(as.numeric(Dx[k,i]<Dx[j,i]))
+#      })
+#    })
+#  })
+#  D = CppS(n2,Kz,Wz,Kyz,Wyz,L)
 
-  hz1 =  bw.selection(Xtr, Ztr, p1 = 0.05, p2 = 0.4)
-  Kz1 = matrix(sapply(as.matrix(Dz),function(x) ker(x,hz1)),n2,n2)
-  Wz1 = apply(Kz1,1,sum)
+#  hz1 =  bw.selection(Xtr, Ztr, p1 = 0.05, p2 = 0.4)
+#  Kz1 = matrix(sapply(as.matrix(Dz),function(x) ker(x,hz1)),n2,n2)
+#  Wz1 = apply(Kz1,1,sum)
 
 
-  Pi = replicate(R,{sapply(1:n2, function(i){
-    W = Kz1[i,]
-    sample((1:n2), 1, prob = W/sum(W))}
-  )}
-  )
-  D1 = resample(n2,Kz,Wz,Kyz,Wyz,L,Pi)
-  pval = (sum(D1>D)+1)/(R+1)
-  return(pval)
-}
+#  Pi = replicate(R,{sapply(1:n2, function(i){
+#    W = Kz1[i,]
+#    sample((1:n2), 1, prob = W/sum(W))}
+#  )}
+#  )
+#  D1 = resample(n2,Kz,Wz,Kyz,Wyz,L,Pi)
+#  pval = (sum(D1>D)+1)/(R+1)
+#  return(pval)
+#}
