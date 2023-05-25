4165bd58818e2952b96772891d08be11967ca2a6
bilol20 <banerjeebilol@gmail.com>
2023-05-25 12:40:47 +0530

    Updated and Running code

--- R/Test.R
+++ R/Test.R
@@ -7,54 +7,16 @@ ker.epa = function(x,s)
   }
 }
 
-ker.normal = function(x,s)
-{
-  return(dnorm(x,0,s))
-}
 
-
-#Stat code correct
-stat = function(n,Dz,hz,Dyz,hyz,L){
+#Stat code not correct
+stat = function(n,Dz,hz,dz,Dyz,hyz,dyz,L){
   Kz = matrix(sapply(as.matrix(Dz),function(x) ker.epa(x,hz)),n,n)
   Kyz = matrix(sapply(as.matrix(Dyz),function(x) ker.epa(x,hyz)),n,n)
-  Wz = apply(Kz,1,sum)
-  Wyz = apply(Kyz,1,sum)
+  Fz = apply(Kz,1,sum)*(1/(n*(hz^dz)))
+  Fyz = apply(Kyz,1,sum)*(1/(n*(hyz^dyz)))
   T = lapply(1:n, function(p){
-    W1 = rep(0,n)
-    W2 = rep(0,n)
-    if(length(which(Kz[p,]!=0))>1&length(which(Kyz[p,]!=0))>1){
-      W1 = Kz[p,]/Wz[p]
-      W2 = Kyz[p,]/Wyz[p]
-    }
-    if(length(which(Kz[p,]!=0))==1&length(which(Kyz[p,]!=0))>1){
-      x = Dz[p,]
-      x1 = x[-p]
-      m = min(x1)
-      ff = which(x==m)
-      W1[ff] = 1
-      W2 = Kyz[p,]/Wyz[p]
-    }
-    if(length(which(Kz[p,]!=0))>1&length(which(Kyz[p,]!=0))==1){
-      x = Dyz[p,]
-      x1 = x[-p]
-      m = min(x1)
-      ff = which(x==m)
-      W2[ff] = 1
-      W1 = Kz[p,]/Wz[p]
-    }
-    if(length(which(Kz[p,]!=0))==1&length(which(Kyz[p,]!=0))==1){
-      x = Dz[p,]
-      x1 = x[-p]
-      m = min(x1)
-      ff = which(x==m)
-      W1[ff] = 1
-      x = Dyz[p,]
-      x1 = x[-p]
-      m = min(x1)
-      ff = which(x==m)
-      W2[ff] = 1
-    }
-
+    W1 = Kz[p,]/sum(Kz[p,])
+    W2 = Kyz[p,]/sum(Kyz[p,])
     S1 = W1%*%t(W1)
     S2 = W2%*%t(W2)
     Q = lapply(1:n, function(i){
@@ -63,7 +25,9 @@ stat = function(n,Dz,hz,Dyz,hyz,L){
     })
     return(sum(unlist(Q)))
   })
-  return(sum(unlist(T)/n))
+  G = unlist(T)
+  G = G*Fz^4*Fyz^4
+  return(sum(G)/n)
 }
 
 cBD.test = function(X, Y, Z, R=500){
@@ -79,41 +43,43 @@ cBD.test = function(X, Y, Z, R=500){
   hyz = quantile(Dyz,0.5)*n^(-1/(dyz+2))
   Kz = matrix(sapply(as.matrix(Dz),function(x) ker.epa(x,hz)),n,n)
   Kyz = matrix(sapply(as.matrix(Dyz),function(x) ker.epa(x,hyz)),n,n)
-  Wz = apply(Kz,1,sum)
-  Wyz = apply(Kyz,1,sum)
 
   L = lapply(1:n, function(i){
     sapply(1:n, function(j){
       return(as.numeric(Dx[,i]<Dx[j,i]))
     })
   })
-  D = stat(n,as.matrix(Dz),hz,as.matrix(Dyz),hyz,L)
+  D = stat(n,as.matrix(Dz),hz,dz,as.matrix(Dyz),hyz,dyz,L)
 
+  #hz1 = bw.selection(X, Z, p1 =0, p2 = 0.5)
 
-  hz1 = quantile(Dz,0.5)*n^(-1/3)
-  Kz1 = matrix(sapply(as.matrix(Dz),function(x) ker.epa(x,hz)),n,n)
-  Wz1 = apply(Kz1,1,sum)
-
+  hz1 = median(Dz)*n^(-1/3)
+  Kz1 = matrix(sapply(as.matrix(Dz),function(x) ker.epa(x,hz1)),n,n)
 
   Pi = replicate(R,{sapply(1:n, function(i){
     W = Kz1[i,]
-    return(sample((1:n), 1, prob = W/sum(W)))
+    if(sum(W!=0)==1){
+      u = as.matrix(Dz)[i,]
+      u = u[-i]
+      m = min(u)
+      return(which(u==m))
+    }else{
+      return(sample((1:n)[-i], 1, prob = W[-i]/sum(W[-i])))
+    }
     }
   )}
   )
   D1 = numeric(R)
+  L1 = list()
+
   for(i in 1:R){
     s = Pi[,i]
-    X1 = X[s,]
-    Dx1 = as.matrix(dist(X1))
-    L1 = lapply(1:n, function(i){
-      sapply(1:n, function(j){
-        return(as.numeric(Dx1[,i]<Dx1[j,i]))
-      })
-    })
-    D1[i] = stat(n,as.matrix(Dz),hz,as.matrix(Dyz),hyz,L1)
+    for(k in 1:n){
+      L1[[k]] = L[[s[k]]][s,s]
+    }
+    D1[i] = stat(n,as.matrix(Dz),hz,dz,as.matrix(Dyz),hyz,dyz,L1)
   }
-  pval = (sum(D1>D)+1)/(R+1)
+  pval = (sum(D1==D) + sum(D1>D)+1)/(R+1)
   return(pval)
 }
 
